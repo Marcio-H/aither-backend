9316601f7e29cb6d1207fa99df9803d68f2ee62b
Marcio Tasca Junior <marcio.junior@triersistemas.com.br>
2022-08-18 08:34:41 -0300

    Primeiro commit

---
+++ src/test/java/br/com/ifsc/aither/backend/AitherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsc.aither.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
